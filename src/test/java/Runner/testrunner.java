b56032c7e938e15b2499239271e8e95b4e3e6e49
vino9k@gmail.com <Govindaswamy@govind>
2020-10-18 01:29:19 +0100

    Feature file and validation for login validation

---
+++ src/test/java/Runner/testrunner.java
@@ -0,0 +1,16 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "src/test/java/Features",
+plugin = { "pretty", "html:target/htmlreports" }
+,glue= {"SeleniumGlueCode"}
+)
+
+public class testrunner {
+
+}
